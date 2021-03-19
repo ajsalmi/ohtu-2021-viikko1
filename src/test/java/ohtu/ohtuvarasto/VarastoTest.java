5b43ba5c9c0c9eeb9f3ed9da7e06ccc0aff7189c
unknown <3845445+ajsalmi@users.noreply.github.com>
2021-03-20 00:28:23 +0200

    pääluokka omaan pakkaukseen

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -1,14 +1,14 @@
 package ohtu.ohtuvarasto;
 
-import org.junit.*;
+//import org.junit.*;
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.AfterClass;
+//import org.junit.After;
+//import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
+//import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
+//import static org.junit.Assert.*;
 
 public class VarastoTest {
 
